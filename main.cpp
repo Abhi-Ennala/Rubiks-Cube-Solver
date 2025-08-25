adee8b2113cf7f66b7d5c322c23b50b11cde6f23
Abhinav Ennala <abhi.ennala@gmail.com>
2025-08-25 18:37:27 +0530

    included solvers in main.cpp file

--- main.cpp
+++ main.cpp
@@ -2,28 +2,25 @@
 #include "Model/RubiksCube3dArray.cpp"
 #include "Model/RubiksCube1dArray.cpp"
 #include "Solver/BFSSolver.h"
+#include "Solver/DFSSolver.h"
+#include "Solver/IDDFSSolver.h"
 #include "Model/RubiksCube.cpp"
 
 using namespace std;
 
 int main(){
-  RubiksCube1dArray cube1;
+   RubiksCube3dArray cube;
+   cube.print();
 
-  // vector<RubiksCube::MOVE> shuffle_moves = cube1.randomShuffleCube(5);
-  // for(auto move: shuffle_moves) cout << cube1.getMove(move) << " ";
-  // cout << "\n";
-  cube1.b();
-  cube1.u();
-  cube1.f();
-  cube1.d();
-  cube1.b();
-  cube1.print();
+   vector<RubiksCube::MOVE> shuffle_moves = cube.randomShuffleCube(6);
+   for (auto move: shuffle_moves) cout << cube.getMove(move) << " ";
+   cout << "\n";
+   cube.print();
 
-  BFSSolver<RubiksCube1dArray, Hash1d> bfsSolver(cube1);
-  vector<RubiksCube::MOVE> solve_moves = bfsSolver.solve();
+   DFSSolver<RubiksCube3dArray, Hash3d> dfsSolver(cube, 8);
+   vector<RubiksCube::MOVE> solve_moves = dfsSolver.solve();
 
-  for(auto move : solve_moves) cout << cube1.getMove(move) << " ";
-  cout << "\n";
-
-  bfsSolver.rubiksCube.print();
+   for (auto move: solve_moves) cout << cube.getMove(move) << " ";
+   cout << "\n";
+   dfsSolver.rubiksCube.print();
 }
